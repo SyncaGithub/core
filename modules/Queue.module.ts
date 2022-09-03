8618798049be629ba686056178aa7740fde34abd
Shaked Aharon <info@synca.co.il>
2022-09-03 20:29:41 +0300

    Fix typo error in Queue naming

--- modules/Queue.module.ts
+++ modules/Queue.module.ts
@@ -2,14 +2,14 @@ import { DynamicModule, Module } from "@nestjs/common";
 import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({})
-export class QeueModule {
+export class QueueModule {
 	static register(port: number): DynamicModule {
 		return {
-			module: QeueModule,
+			module: QueueModule,
 			imports: [
 				ClientsModule.register([
 					{
-						name: "QEUE_SERVICE",
+						name: "QUEUE_SERVICE",
 						transport: Transport.REDIS,
 						options: {
 							host: "localhost",
